6e873a79f52fdee4ba81309206c04910992b9778
Vimal-Kanzariya2307 <vimalkanzariya2307@gmail.com>
2026-04-07 13:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from services.market_data_service import fetch_stock_data
+from backend.services.market_data_service import fetch_stock_data
 import requests
 
 
